029c3550900ad6a0f1122ff44bffd946d739d6f8
anthonymfscott <anthonymfscott@gmail.com>
2019-10-02 17:08:11 +0200

    Create Xcode project

--- Petroyes/main.swift
+++ Petroyes/main.swift
@@ -2,11 +2,10 @@
 //  main.swift
 //  Petroyes
 //
-//  Created by Tony Scott enfant Depaepe on 02/10/2019.
+//  Created by anthonymfscott on 02/10/2019.
 //  Copyright © 2019 anthonymfscott. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
 
