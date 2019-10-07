e7c73a831593007fd749277cbb5a7af5ab9cc6f9
anthonymfscott <anthonymfscott@gmail.com>
2019-10-07 23:27:38 +0200

    Implement swopsWeapon() Update names of a few class properties to better accommodate swopsWeapon() Update subclasses: add weapon names according to their strength

--- Petroyes/Character.swift
+++ Petroyes/Character.swift
@@ -12,8 +12,10 @@ class Character {
     var name: String?
     var job = ""
     var hp = 0
-    var weapon = ""
-    var strength = [String: Int]()
+    var availableWeapons: [String: String] = [:]
+    var currentWeapon = ""
+    var weaponsStrength: [String: Int] = [:]
+    var currentStrength = 0
     var description = ""
     var isDead = false
     
@@ -30,8 +32,8 @@ class Character {
         }
         print("\n--- \(job) ---"
             + "\nhealth points: \(hp)"
-            + "\nweapon: \(weapon)"
-            + "\nstrength: \(strength["usual"]!)")
+            + "\nweapon: \(currentWeapon)"
+            + "\nstrength: \(currentStrength)")
         if Game.existingCharacterNames.count == 0 {
             print(description)
         }
@@ -43,13 +45,19 @@ class Character {
     
     func action(targetCharacter: Character) {
         print("\(name!) hits \(targetCharacter.name!)!")
-        targetCharacter.hp -= strength["usual"]!
-        print("\(targetCharacter.name!) has lost \(strength["usual"]!) health points!")
+        targetCharacter.hp -= currentStrength
+        print("\(targetCharacter.name!) loses \(currentStrength) health points!")
         if targetCharacter.hp <= 0 {
             targetCharacter.hp = 0
             targetCharacter.isDead = true
             print("\(targetCharacter.name!) is dead!")
         }
+    }
+    
+    func swopsWeapon() -> String {
+        currentWeapon = availableWeapons["diminished"]!
+        currentStrength = weaponsStrength["diminished"]!
+        return "Too bad! \(name!) opens the chest and gets a weaker weapon: \(currentWeapon), strength: \(currentStrength)!"
     }
 }
 
@@ -59,8 +67,10 @@ class Mercenary: Character {
         super.init(name: name)
         job = "mercenary"
         hp = 100
-        weapon = "sword"
-        strength = ["diminished": 5, "usual": 100, "increased": 20]
+        availableWeapons = ["diminished": "knife", "usual": "sword", "increased": "axe"]
+        currentWeapon = availableWeapons["usual"]!
+        weaponsStrength = ["diminished": 5, "usual": 10, "increased": 20]
+        currentStrength = weaponsStrength["usual"]!
         description = "Mercenaries are very tough, and best at close-range fighting using all types of swords."
     }
 }
@@ -70,8 +80,10 @@ class Arbalester: Character {
         super.init(name: name)
         job = "arbalester"
         hp = 90
-        weapon = "crossbow"
-        strength = ["diminished": 5, "usual": 120, "increased": 20]
+        availableWeapons = ["diminished": "darts", "usual": "crossbow", "increased": "superarbalest"]
+        currentWeapon = availableWeapons["usual"]!
+        weaponsStrength = ["diminished": 5, "usual": 12, "increased": 20]
+        currentStrength = weaponsStrength["usual"]!
         description = "Arbalesters use their crossbows to shoot powerful bolts from a distance."
     }
 }
@@ -81,8 +93,10 @@ class Pyromaniac: Character {
         super.init(name: name)
         job = "pyromaniac"
         hp = 80
-        weapon = "blowtorch"
-        strength = ["diminished": 5, "usual": 150, "increased": 20]
+        availableWeapons = ["diminished": "matchbox", "usual": "blowtorch", "increased": "flamethrower"]
+        currentWeapon = availableWeapons["usual"]!
+        weaponsStrength = ["diminished": 5, "usual": 15, "increased": 20]
+        currentStrength = weaponsStrength["usual"]!
         description = "Pyromaniacs just love fire... maybe a little bit too much sometimes."
     }
 }
@@ -92,14 +106,16 @@ class Magus: Character {
         super.init(name: name)
         job = "magus"
         hp = 70
-        weapon = "staff"
-        strength = ["diminished": 5, "usual": 100, "increased": 20]
-        description = "Magi aren't very good at fighting, but excel at healing wounds using the power of their staffs."
+        availableWeapons = ["diminished": "foo-foo dust", "usual": "wand", "increased": "magic staff"]
+        currentWeapon = availableWeapons["usual"]!
+        weaponsStrength = ["diminished": 5, "usual": 10, "increased": 20]
+        currentStrength = weaponsStrength["usual"]!
+        description = "Magi aren't very good at fighting, but excel at healing wounds using the power of their wands."
     }
     
     override func action(targetCharacter: Character) {
         print("\(name!) cures \(targetCharacter.name!)!")
-        targetCharacter.hp += strength["usual"]!
-        print("\(targetCharacter.name!) has recovered \(strength["usual"]!) health points!")
+        targetCharacter.hp += currentStrength
+        print("\(targetCharacter.name!) recovers \(currentStrength) health points!")
     }
 }
