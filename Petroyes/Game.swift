c8e72530aca9385deaf2a66d60b3d11b68c03110
anthonymfscott <anthonymfscott@gmail.com>
2019-10-23 14:29:17 +0200

    Update Game: adapt scope create more smaller functions add comments

--- Petroyes/Game.swift
+++ Petroyes/Game.swift
@@ -8,14 +8,14 @@
 
 import Foundation
 
-final public class Game {
+public final class Game {
     // MARK: Properties
     private var numberOfPlayers: Int
     private var players: [Player] = [] // An array containing the players
     private var numberOfRounds: Int // Counts the number of rounds
     internal static var existingCharacterNames: [String] = [] // Stores the character names that were already created by other players and can't be used again
     
-    // MARK: Initialisation
+    // MARK: Creation of the game
     init(numberOfPlayers: Int) {
         self.numberOfPlayers = numberOfPlayers // Sets a specific number of players
         numberOfRounds = 0 // Resets the number of rounds to 0
@@ -23,96 +23,108 @@ final public class Game {
     
     func startGame() {
         print("WELCOME TO PETROYES!")
-        // Creation of new players:
+        createPlayers()
+        describeJobs()
+        createTeams()
+        fight()
+    }
+    
+    private func createPlayers() {
         for playerNumber in 1...numberOfPlayers {
-            let newPlayer = Player(number: Int(playerNumber))
-            players.append(newPlayer)
+            let newPlayer = Player(number: Int(playerNumber)) // Creates desired number of instances of Player
+            players.append(newPlayer) // Adds each instance to 'players' array
         }
-        describeJobs() // Prints a description of the available character types
-        createTeams() // Creates a team of characters for each player
-        fight() // Starts the game
     }
     
     private func describeJobs() {
-          print("\nHere's a list of the jobs available and their related skills:") // A brief description for the player
-          let playableCharacters = [Mercenary(), Arbalester(), Pyromaniac(), Magus()] // An array containing all available characters without names
-          for playableCharacter in playableCharacters { // A for-in that goes through the 'playableCharacters' array
-              playableCharacter.describe() // Calls the so-called method for each character
-          }
-      }
+        print("\nHere's a list of the jobs available and their related skills:")
+        let playableCharacters = [Mercenary(), Arbalester(), Pyromaniac(), Magus()] // Creates an array containing instances of all available characters without names
+        for playableCharacter in playableCharacters {
+            playableCharacter.describe() // Calls the 'describe' method for each character
+        }
+    }
     
     private func createTeams() {
-        for player in players { // A for-in that goes through the 'players' array
-            player.selectCharacters()
+        for player in players {
+            player.selectCharacters() // Calls the 'selectCharacters' method for each player
         }
         print("\nHere are the finalised teams:")
         for player in players {
-            player.showStats()
+            player.showStats() // Calls the 'showStats' method for each player
         }
     }
     
-    // MARK: Fight
+    // MARK: Fight phase
     private func fight() {
         print("\nNow let the fight begin!")
-        var remainingPlayers = players
-        while numberOfPlayers > 1 {
-            numberOfRounds += 1
-            for activePlayer in remainingPlayers {
-                if activePlayer.teamIsDead() {
-                    print("\n\(activePlayer.name.uppercased())'S TEAM IS DEAD.")
-                } else {
-                    print("\n\(activePlayer.name.uppercased())'S TURN:")
-                    if numberOfRounds > 3 && chestAppears() {
-                        activePlayer.chest()
-                    }
-                    if let ally = activePlayer.selectAlly() {
-                        if ally.job == "magus" {
-                            if let allyTarget = activePlayer.selectAlly() {
-                                ally.action(targetCharacter: allyTarget)
-                            }
-                        } else {
-                            if let enemy = activePlayer.selectEnemy(amongst: remainingPlayers) {
-                                ally.action(targetCharacter: enemy)
-                            }
-                        }
+        var remainingPlayers = players // Creates a copy of the 'players' array, to be modified afterwards
+        while numberOfPlayers > 1 { // As long as there's more than a player left:
+            round(remainingPlayers: remainingPlayers) // - goes through a round of fighting with the remaining players
+            remainingPlayers = removeDeadTeams(remainingPlayers: remainingPlayers) // - deletes the players whose teams are dead from the 'remainingPlayers' array
+        }
+        endGame(remainingPlayers: remainingPlayers) // Ends the game
+    }
+    
+    private func round(remainingPlayers: [Player]) {
+        numberOfRounds += 1 // Increments the number of rounds
+        
+        for activePlayer in remainingPlayers { // For every remaining player in turn:
+            if activePlayer.teamIsDead() { // - checks if the player's team is dead and prints a message if it's the case
+                print("\n\(activePlayer.name.uppercased())'S TEAM IS DEAD.")
+            } else { // (if the player's team isn't dead:)
+                print("\n\(activePlayer.name.uppercased())'S TURN:")
+                
+                if numberOfRounds > 3 && chestAppears() {
+                    activePlayer.manageChest() // - calls the 'manageChest' method if a chest appears (option available after the third round only)
+                }
+                
+                if let activeCharacter = activePlayer.selectAlly() { // - selects a character from the player's team to play the next action
+                    if activeCharacter.job == "magus",
+                        let ally = activePlayer.selectAlly() { // - selects an ally from the player's team (if the action is taken by a magus)
+                        activeCharacter.takeAction(targetCharacter: ally) // - performs the action on the ally
+                    } else if let enemy = activePlayer.selectEnemy(amongst: remainingPlayers) { // - selects an enemy amongst the remaining players' teams
+                        activeCharacter.takeAction(targetCharacter: enemy) // - performs the action on the enemy
                     }
                 }
             }
-            print("\nEnd of round \(numberOfRounds):")
-            for player in players {
-                player.showStats()
-            }
-            remainingPlayers = removeDeadTeams(remainingPlayers: remainingPlayers)
         }
-        print("""
-            \nGAME OVER!
-            \(remainingPlayers[0].name) wins the battle in \(numberOfRounds) rounds!\n\n
-            """)
+        print("\nEnd of round \(numberOfRounds):")
+        
+        for player in players {
+            player.showStats() // Shows statistics at the end of the round
+        }
     }
     
     private func chestAppears() -> Bool {
-        let randomNumber = Int.random(in: 0...2)
+        let randomNumber = Int.random(in: 0...2) // Chooses a random number between 0, 1 and 2
         if randomNumber == 0 {
-            return true
+            return true // Returns true if the number is 0
         } else {
-            return false
+            return false // Returns false in the two other cases (1 chance out of 3 for a chest to appear)
         }
     }
     
     private func removeDeadTeams(remainingPlayers: [Player]) -> [Player] {
         var remainingPlayers = remainingPlayers
-        var numberOfDeadTeams = 0
-        var deadTeamIndices: [Int] = []
-        for index in 0...remainingPlayers.count-1 {
-            if remainingPlayers[index].team[0].isDead && remainingPlayers[index].team[1].isDead && remainingPlayers[index].team[2].isDead {
-                numberOfDeadTeams += 1
-                deadTeamIndices.append(index)
+        var numberOfDeadTeams = 0 // Resets the count of dead teams to 0
+        var deadTeamsIndices: [Int] = [] // Creates an empty array for all dead teams' indices
+        for index in 0...remainingPlayers.count-1 { // For each player of the last round:
+            if remainingPlayers[index].team[0].isDead && remainingPlayers[index].team[1].isDead && remainingPlayers[index].team[2].isDead { // (if all characters in the team are dead)
+                numberOfDeadTeams += 1 // - records a dead team
+                deadTeamsIndices.append(index) // - records the index of the KO player
             }
         }
-        numberOfPlayers -= numberOfDeadTeams
-        for index in deadTeamIndices {
-            remainingPlayers.remove(at: index)
+        numberOfPlayers -= numberOfDeadTeams // Takes away the number of players that corresponds to the number of dead teams
+        for index in deadTeamsIndices {
+            remainingPlayers.remove(at: index) // Removes the KO players at the indices that were recorded above
         }
-        return remainingPlayers
+        return remainingPlayers // Returns the modified array
+    }
+    
+    private func endGame(remainingPlayers: [Player]) {
+        print("""
+            \nGAME OVER!
+            \(remainingPlayers[0].name) wins the battle in \(numberOfRounds) rounds!\n\n
+            """) // Prints the main statistics of the game, with the only player left in the array
     }
 }
